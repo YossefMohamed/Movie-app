63b68ec1ba5db759f00033d948515c0826d010c5
Yossef Mohamed <47724276+YossefMohamed@users.noreply.github.com>
2023-02-15 19:43:31 +0200

    change graphql api url

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://movie-graph-api.herokuapp.com/api/graphql",
+  uri: "https://movie-app-api-huxd.onrender.com/api/graphql",
   cache: new InMemoryCache(),
 });
 
